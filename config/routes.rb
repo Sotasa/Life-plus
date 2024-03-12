28219652435e2d5311d10c4c8e8aaf1679dc53bc
Sota Sato <22de037j@rikkyo.ac.jp>
2024-03-12 18:30:58 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   get 'blogs/index'
   devise_for :users
 
-  
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+
   resources :blogs
  
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
