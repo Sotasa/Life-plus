155efe0fad73f578abdc8daeb440ef77bff983fb
Sota Sato <22de037j@rikkyo.ac.jp>
2024-03-12 19:30:09 +0900

    fourth commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,8 @@ class CommentsController < ApplicationController
     before_action :authenticate_user!
 
   def create
-    habit =:Habit.find(params[:habit_id])
-    comment = habit.comments.build(comment_params) #buildを使い、content:habit_idの二つを同時に代入
+    habit =Habit.find(params[:habit_id])
+    comment =habit.comments.build(comment_params) #buildを使い、content:habit_idの二つを同時に代入
     comment.user_id = current_user.id
     if comment.save
       flash[:success] = "コメントしました"
@@ -13,4 +13,11 @@ class CommentsController < ApplicationController
       redirect_back(fallback_location: root_path) #直前のページにリダイレクト
     end
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
 end
